976025ef514a076b386b0e66dccd34f3a66bc454
Daniyal Khan <d4n.kh4n@gmail.com>
2021-11-15 13:51:14 +0500

    Adding Form Data Function

---
+++ sc.js
@@ -0,0 +1,11 @@
+var selectedRow = null;
+function onFormSubmit() {}
+// Retrive the data
+function readFromData() {
+  var formData = {};
+  formData["productCode"] = document.getElementById("productCode").value;
+  formData["product"] = document.getElementById("product").value;
+  formData["qty"] = document.getElementById("qty").value;
+  formData["perPrice"] = document.getElementById("perPrice").value;
+  return formData;
+}
